d79af58fd20018175505a45cbfc6b656e833b7ba
Pawel Krol <paullkroll@icloud.com>
2023-11-26 13:49:11 +0100

    Add phone-number and email adress, end task

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ const femaleNames = ["Agata", "Alicja", "Elżbieta", "Patrycja", "Edyta", "Micha
 
 const lastNames = ["Krupnik", "Kownacka", "Ładzik", "Sobek", "Małysiak", "Zgoda", "Drążek", "Foks", "Pulit", "Hutyra"];
 
+const phoneNumbers = [
+    "505-505-505", 
+    "543-564-789", 
+    "665-440-667", 
+    "767-456-243", 
+    "676-678-980", 
+    "430-533-234", 
+    "098-564-234", 
+    "776-556-909", 
+    "675-345-555",
+    "786-564-798"
+]
+
 const randChoice = (arr) => {
     const randomIndex =  Math.floor(Math.random() * arr.length);
     return arr[randomIndex];
@@ -23,15 +36,20 @@ for (let i = 0; i < 20; i++ ) {
         selectedName = randChoice(maleNames);
     } else {
         selectedName = randChoice(femaleNames);
-    }
+    };
 
+    const selectedLastName = lastNames[Math.floor(Math.random () * lastNames.length)];
     const selectedAge = Math.floor(Math.random() * (65 - 18 + 1) + 18);
+    const selectedPhoneNumber = randChoice(phoneNumbers);
+    const selectedMailAdress = selectedName + '.' + selectedLastName + '@gmail.com';
 
     const newObject = {
         gender: selectedGender,
         firstName: selectedName,
-        lastName: randChoice(lastNames),
-        age: selectedAge
+        lastName: selectedLastName,
+        age: selectedAge,
+        phone: selectedPhoneNumber,
+        mail: selectedMailAdress
     };
 
     people.push(newObject);
